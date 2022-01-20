fb307a245e7271e2dad641294c0d5ac7151cf1b5
Nicholas <romitinicholas@gmail.com>
2022-01-20 18:44:51 +0100

    prima versione filtro

--- js/main.js
+++ js/main.js
@@ -1,11 +1,24 @@
 const app = new Vue ({
     el: "#app",
     data: {
-        dischi: []
+        dischi: [],
+        dischi2: [],
+        genre: ""
     },
     created() {
         axios.get("http://localhost:8888/php-ajax-dischi/index.php").then( (response) => {
             this.dischi = response.data;
         });
+    },
+    methods: {
+        selectByGenre() {
+            axios.get("http://localhost:8888/php-ajax-dischi/index.php").then( () => {
+                this.dischi2 = [];
+                this.dischi.forEach(element => {
+                    if (element.genre == this.genre) 
+                    this.dischi2.push(element);
+                });
+            });
+        }
     }
 });
